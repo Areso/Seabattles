75e01efbabc662a56b05b400563f55db0ed28060
Anton Gladyshev <returnstrike@yandex.ru>
2017-03-24 01:58:53 +0000

    ticks done!

--- click.py
+++ click.py
@@ -7,24 +7,40 @@ cnx = mysql.connector.connect(user='root', password='0000',
                               database='seabattles')
 restart = 0;
 try:
-   print("in try")
+   #print("in try")
    while (restart == 0):
-      print("in while")
-      cursor = cnx.cursor()
+      #print("in while")
+      cursor_serv = cnx.cursor()
       cnx.autocommit=True
-      #cnx.autocommit(True)# for Python 3+
-      cursor.execute("""
+      query_serv = """
          select * from server
-      """)
-      print("in for")
-      result = cursor.fetchall()
-      for row in result:
-          speed=row[1]
-          if speed != speed:
-              speed = speed
-          print(speed)
-      restart = 1        
+      """ 
+      cursor_serv.execute(query_serv)
+      result_serv = cursor_serv.fetchall()
+      for row_serv in result_serv:
+          #restart=row[1]
+          if restart != row_serv[1]:
+              restart = row_serv[1]
+              print(restart)
+
+      cursor_players = cnx.cursor()
+      #cnx.autocommit=True
+      query_players = """
+         select * from players
+      """
+      cursor_players.execute(query_players)
+      result_players = cursor_players.fetchall()
+      for row_players in result_players:
+          gold=row_players[3]+1
+          print(gold)
+          query_player_update = """
+             UPDATE players SET gold=%s WHERE player_id=%s
+          """
+          t = (gold, row_players[0])
+          cursor_players.execute(query_player_update, t)
+           #update players set gold=
+      #restart = 1        
 finally:
-    cnx.close()    
-print("hello")
-print(v)
+    cnx.close()
+print("server has been stopped")
+
